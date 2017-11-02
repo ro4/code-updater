047c6c593af749755c75cdbc49566fe4ddf08d36
ro4 <thefrp@gmail.com>
2017-11-02 17:36:52 +0800

    init

---
+++ run.php
@@ -0,0 +1,41 @@
+<?php
+require __DIR__ . '/bootstrap/app.php';
+use GitWrapper\GitWrapper;
+
+$config   = require_once('config.php');
+$wrapper  = new GitWrapper();
+$port     = isset($config['port']) ? $config['port'] : 4000;
+$projects = isset($config['projects']) ? $config['projects'] : [];
+$password = isset($config['password']) ? $config['password'] : [];
+$http     = new swoole_http_server("0.0.0.0", $port);
+
+$http->on("start", function ($server) use ($port) {
+    echo "code updater is starting at port:{$port}\n";
+});
+
+$http->on("request", function ($request, $response) use ($wrapper, $projects, $password) {
+    $response->header("Content-Type", "application/json");
+    $method = $request->server['request_method'];
+    $path   = $request->server['path_info'];
+    if ($method == 'POST' && $path == '/push') {
+        $project       = isset($request->post['project']) ? $request->post['project'] : null;
+        $passwordKey   = isset($password['key']) ? $password['key'] : null;
+        $passwordValue = isset($password['value']) ? $password['value'] : null;
+        $password      = isset($request->post[$passwordKey]) ? $request->post[$passwordKey] : null;
+        $path          = isset($projects[$project]['path']) ? $projects[$project]['path'] : null;
+        $pass          = ($password && $password == $passwordValue);
+        if ($pass && $path) {
+            echo date('[Y-m-d H:i:s]:') . $project . ' ' . $wrapper->workingCopy($path)->pull();
+            $response->status(200);
+            $response->end(json_encode(['msg' => 'ok']));
+        } else {
+            $response->status(500);
+            $response->end(json_encode(['msg' => 'failed']));
+        }
+    } else {
+        $response->status(404);
+        $response->end(json_encode(['msg' => 'not found']));
+    }
+});
+
+$http->start();
